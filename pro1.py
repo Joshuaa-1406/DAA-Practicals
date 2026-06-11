89567c80f285a29cfabcaa0f3fd4425f9ec8807a
Joshuaa-1406 <joshua7sfrank@gmail.com>
2026-06-11 16:52:43 +0000

    Refactor linear search to return detailed results and add example usage

--- pro1.py
+++ pro1.py
@@ -1,6 +1,13 @@
-#Linear Search Program
 def linear_search(arr, target):
     for i in range(len(arr)):
         if arr[i] == target:
-            return i  # Return the index of the target element
-    return -1  # Return -1 if the target element is not found
+            c = i + 1
+            case ="Best Case 0(1)" if c == 1 else "Worst Case O(n)" if c == len(arr) else "Average Case O(n)"
+            return f"Element found at index {i} comparisons ({c}) \n{case}"
+    return "Element not found in the array."
+
+# Example usage
+arr = [5, 3, 2, 8, 1]
+target = int(input("Enter the target element: "))
+result = linear_search(arr, target)
+print(result)
