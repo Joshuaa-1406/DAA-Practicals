a4533ce367b3b4168a97d2bafaf1dbabb767a45e
Joshuaa-1406 <joshua7sfrank@gmail.com>
2026-06-12 17:29:37 +0000

    Prim's Algorithm (MST)

--- pro10.py
+++ pro10.py
@@ -1,27 +1,16 @@
-import sys
+def prims(g):
+    n = len(g)
+    sel = [False] * n
+    sel[0] = True
+    for _ in range(n - 1):
+        mn, x, y = float('inf'), 0, 0
+        for i in range(n):
+            if sel[i]:
+                for j in range(n):
+                    if not sel[j] and 0 < g[i][j] < mn:
+                        mn, x, y = g[i][j], i, j
+        print(f"{x} - {y} : {mn}")
+        sel[y] = True
 
-def prims(graph):
-    v = len(graph)
-    visited = [False] * v
-    visited[0] = True
-    print("Edge : Weight")
-    for _ in range(v - 1):
-        min_w,x,y = sys.maxsize,0,0
-        for i in range(v):
-            if visited[i]:
-                for j in range(v):
-                    if not visited[j] and graph[i][j] != 0:
-                        if graph[i][j] < min_w:
-                            min_w,x,y = graph[i][j],i,j
-        visited[y] = True
-        print(f"{x} - {y} : {graph[x][y]}")
-    print("ETC: O(V^2)")
-
-graph = [
-    [0, 2, 0, 6, 0],
-    [2, 0, 3, 8, 5],
-    [0, 3, 0, 0, 7],    
-    [6, 8, 0, 0, 9],
-    [0, 5, 7, 9, 0]
-]   
+graph = [[0,2,0,6,0],[2,0,3,8,5],[0,3,0,0,7],[6,8,0,0,9],[0,5,7,9,0]]
 prims(graph)
